4044b0b44c778fdfe688479eab00f114cf00e0b9
Michael Bleigh <michael@intridea.com>
2010-10-15 19:06:26 -0500

    Initial import. Blank rails app FTW.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsWizard::Application.config.secret_token = 'a35f4c116084ef7f5029e450102daf70861c5416257b91d9e6c3a272eb3e67a057691e7ec2f82339ccd02e539db7bd358a4e5cfc7161b4a7e751b712fccce499'
